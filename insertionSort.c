55c692067f7a0165c0226a12aed5bea7212a5fe1
blitzboi <146631108+blitz31@users.noreply.github.com>
2024-04-28 11:55:59 +0530

    Create insertionSort.c

---
+++ insertionSort.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#define max 50
+int arr[max];
+
+void insertionSort(int n){
+    int i,j,temp;
+    for(i=0; i<n-1; i++){
+        for(j=i+1; j>0; j--){
+            if(arr[j-1] > arr[j]){
+                temp = arr[j];
+                arr[j] = arr[j-1];
+                arr[j-1] = temp;
+            }
+            else{
+                break;
+            }
+        }
+    }
+}
+
+void main(){
+    int i, n;
+    printf("Enter the size of an array\n");
+    scanf("%d", &n);
+    printf("Enter the elements\n");
+    for(i=0; i<n; i++){
+        scanf("%d", &arr[i]);
+    }
+    insertionSort(n);
+    printf("After sorting\n");
+    for(i=0; i<n; i++){
+        printf("%d ", arr[i]);
+    }
+}
